c11da510ee8a7fabd86c4a8ac6e015256e2a8f5b
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-11-09 13:55:42 -0800

    login page is styled and is fetching data when the login info is correct

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { useState } from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
-
-const Login = ()=> {
-  return (<h2>Login</h2>)
-}
+// Components
+import NavBar from "./components/NavBar";
+import Login from "./components/Login";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <h2>Client Auth Project</h2>
+      <NavBar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+
+      <Switch>
+        <Redirect exact path="/" to="/login" />
+        <Route exact path="/login">
+          <Login setIsLoggedIn={setIsLoggedIn} />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ friends/src/components/NavBar.js
@@ -0,0 +1,77 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormGroup from "@mui/material/FormGroup";
+import MenuItem from "@mui/material/MenuItem";
+import Menu from "@mui/material/Menu";
+
+export default function NavBar({ isLoggedIn, setIsLoggedIn }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = (e) => {
+    if (e.target.textContent === "Logout") setIsLoggedIn(false);
+
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Friends Viewer
+          </Typography>
+          {isLoggedIn && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem
+                  aria-label="logout button"
+                  value="logout"
+                  onClick={handleClose}
+                >
+                  Logout
+                </MenuItem>
+                <MenuItem onClick={handleClose}>View Friends</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
